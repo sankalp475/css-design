b79d1e9587cc1105759c50ef5695f2fb94d2cdc8
sankalp <sankalp05oct@gmail>
2023-10-25 23:57:58 +0530

    1698258469377

--- src/js/script.js
+++ src/js/script.js
@@ -1,20 +1,20 @@
-// const ulElement = document.querySelector("ul[class='list-item-container']")
-// // var json2html = require('json-to-html')
-// const fetchJson = async (handler) => {
-//     const res = await fetch('project.json')
-//     const JsonData = await res.json()
-//     return (handler instanceof Function) ? handler(JsonData) : JsonData
-// }
-// let i = 0;
-// const JsonToHtml = (jsonData) => {
-
-// }
-// fetchJson(JsonToHtml)
+const ulElement = document.querySelector("ul[class='list-item-container']")
+// var json2html = require('json-to-html')
+const fetchJson = async (handler) => {
+    const res = await fetch('project.json')
+    const JsonData = await res.json()
+    return (handler instanceof Function) ? handler(JsonData) : JsonData
+}
+let i = 0;
+const JsonToHtml = (jsonData) => {
+    console.log(jsonData)
+}
+fetchJson(JsonToHtml)
 
 window.onload = () => {
     window.onscroll = (e) => {
         let current = window.scrollY;
-        console.log(current)
+        // console.log(current)
         let text = document.querySelector('h1[class="htitle"]');
         if (current > 50 || current > 50) {
             text.style.fontSize = `3.51rem`;
